6e9437e9bf48b257354168ef4d7762a7455e016f
Philipp A <flying-sheep@web.de>
2019-03-06 12:05:09 +0100

    added packages to setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/zsh
 
+test -f pacman && pacman -Qq >/dev/null \
+    prezto \
+    thefuck undistract-me-git pkgfile \
+    bat exa \
+    ksshaskpass
+
 ln -s .config/dotfiles/zshrc ~/.zshrc
 ln -s .config/dotfiles/env.sh ~/.zshenv
-ln -s ../../dotfiles/env.sh .config/plasma-workspace/env/
+ln -s ../../dotfiles/env.sh ~/.config/plasma-workspace/env/env.sh
 ln -s .config/dotfiles/zpreztorc ~/.zpreztorc 
 
-ln -s ../dotfiles/gitconfig git/config
-ln -s ../dotfiles/fonts.conf .config/fontconfig/
+ln -s ../dotfiles/gitconfig ~/.config/git/config
+ln -s ../dotfiles/fonts.conf ~/.config/fontconfig/fonts.conf
 ln -s .config/dotfiles/vimrc ~/.vimrc
