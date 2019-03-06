663b07c3288c5b749b13de279dc3348eb9674a43
Philipp A <flying-sheep@web.de>
2019-03-06 11:45:42 +0100

    fontconfig

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ ln -s ../../dotfiles/env.sh .config/plasma-workspace/env/
 ln -s .config/dotfiles/zpreztorc ~/.zpreztorc 
 
 ln -s ../dotfiles/gitconfig git/config
+ln -s ../dotfiles/fonts.conf .config/fontconfig/
 ln -s .config/dotfiles/vimrc ~/.vimrc
